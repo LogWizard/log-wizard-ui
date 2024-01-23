b9b0683c5f5d5370944d781736d8af3d6df1250a
Павло Стужук <pasha_klaspb@ukr.net>
2024-01-23 22:35:42 +0200

    feat(ui): add window title for log-wizard-ui
    This commit adds a window title for the log-wizard-ui by writing an escape sequence to stdout. The escape sequence '\x1b]2;log-wizard-ui\x1b\x5c' sets the window title to "log-wizard-ui". This enhancement improves the user experience by providing a descriptive title for the application window.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import { createMessageServer } from './src/server.js';
+process.stdout.write('\x1b]2;log-wizard-ui\x1b\x5c');
 createMessageServer();
 export * from '../log-wizard-ui/src/server.js';
